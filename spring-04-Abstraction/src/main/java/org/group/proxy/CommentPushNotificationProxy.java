5b84adb7683f2d76441a6505e9f76c694f23b2ea
drenzz1 <you@example.com>
2023-12-27 22:20:44 +0100

    Qualifier

--- spring-04-Abstraction/src/main/java/org/group/proxy/CommentPushNotificationProxy.java
+++ spring-04-Abstraction/src/main/java/org/group/proxy/CommentPushNotificationProxy.java
@@ -2,14 +2,13 @@ package org.group.proxy;
 
 import org.group.entity.Comment;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("email")
-public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+@Qualifier("push")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Sending notification for connect : "+comment.getText());
+        System.out.println("Sending push notification for comment :" +comment.getText());
     }
 }
--- spring-04-Abstraction/src/main/java/org/group/service/CommentService.java
+++ spring-04-Abstraction/src/main/java/org/group/service/CommentService.java
@@ -4,6 +4,7 @@ import org.group.entity.Comment;
 import org.group.proxy.CommentNotificationProxy;
 import org.group.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +14,7 @@ public class CommentService {
     private final CommentNotificationProxy commentNotificationProxy;
 
     @Autowired
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("push") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
