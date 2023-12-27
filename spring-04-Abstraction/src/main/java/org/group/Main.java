716bad8202201db958f1bf779a02f51675cec93e
drenzz1 <you@example.com>
2023-12-27 22:01:37 +0100

    Abstraction

--- spring-04-Abstraction/src/main/java/org/group/Main.java
+++ spring-04-Abstraction/src/main/java/org/group/Main.java
@@ -1,9 +1,20 @@
 package org.group;
 
+import org.group.config.ProjectConfig;
 import org.group.entity.Comment;
+import org.group.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+
         Comment comment = new Comment();
+        comment.setAuthor("OZZY");
+        comment.setText("Spring framework");
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        CommentService commentService = container.getBean(CommentService.class);
+        commentService.publishComment(comment);
     }
 }
