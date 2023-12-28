249944ba506f3f003d45c60b02d3be127dc19556
drenzz1 <you@example.com>
2023-12-28 22:45:29 +0100

    Spring Boot Demo

--- spring-05-core-beanscope/src/main/java/org/example/service/CommentService.java
+++ spring-05-core-beanscope/src/main/java/org/example/service/CommentService.java
@@ -5,24 +5,23 @@ package org.example.service;
 import org.example.entity.Comment;
 import org.example.proxy.CommentNotificationProxy;
 import org.example.repository.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope("prototype")
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Scope("prototype")
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    @Autowired
+
     public CommentService(CommentRepository commentRepository, @Qualifier("push") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Hello");
     }
 
     public void publishComment(Comment comment){
