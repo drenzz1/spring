dc34fb872079eeef47bfec838bf61a1c2e2b33b2
drenzz1 <you@example.com>
2024-02-01 22:52:47 +0100

    Starting with Model

---
+++ spring-08-spring-MVC-Model/src/test/java/org/foo/Spring08SpringMvcModelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08SpringMvcModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
