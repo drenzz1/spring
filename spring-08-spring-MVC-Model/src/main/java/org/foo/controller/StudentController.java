51df65dde96e080419f1a8889a88f2a0464fbbb8
drenzz1 <you@example.com>
2024-02-01 23:08:41 +0100

    SPRING MVC- MODEL

---
+++ spring-08-spring-MVC-Model/src/main/java/org/foo/controller/StudentController.java
@@ -0,0 +1,20 @@
+package org.foo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+    @RequestMapping("/welcome")
+    public String homePage(Model model){
+
+        model.addAttribute("name","Cydeo");
+        model.addAttribute("course","MVC");
+
+        return "student/welcome";
+    }
+
+
+}
