0e61215627bb8e25feae5355e896d79f8ffbe51f
drenzz1 <you@example.com>
2024-02-02 13:55:12 +0100

    SPRING MVC- MODEL

--- spring-08-spring-MVC-Model/src/main/java/org/foo/controller/StudentController.java
+++ spring-08-spring-MVC-Model/src/main/java/org/foo/controller/StudentController.java
@@ -5,13 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model){
-
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+
+        List<String > dreni = new ArrayList<>();
+        for (int i =0 ; i<1000; i++  ){
+            dreni.add("student id " + i);
+        }
+        int dreni2 = new Random().nextInt(1,1000);
+        String subject = "collections";
+
+        model.addAttribute("subject", subject);
+        model.addAttribute("student_id", dreni );
+        model.addAttribute("dreni2" , dreni2);
+
 
         return "student/welcome";
     }
