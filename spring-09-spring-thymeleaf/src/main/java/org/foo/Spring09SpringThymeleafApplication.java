3d3c969a1d3052557043fd319a8ef766cf2607c4
drenzz1 <you@example.com>
2024-02-03 00:24:34 +0100

    Thymeleaf

---
+++ spring-09-spring-thymeleaf/src/main/java/org/foo/Spring09SpringThymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09SpringThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09SpringThymeleafApplication.class, args);
+    }
+
+}
