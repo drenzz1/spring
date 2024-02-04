d19bef8d402b487692131cd89c35ca85179c94a9
drenzz1 <you@example.com>
2024-02-04 23:35:31 +0100

    Thymeleaf

--- spring-09-spring-thymeleaf/src/main/java/org/foo/bootstrap/DataGenerator.java
+++ spring-09-spring-thymeleaf/src/main/java/org/foo/bootstrap/DataGenerator.java
@@ -4,16 +4,15 @@ import com.github.javafaker.Faker;
 import org.foo.model.Student;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DataGenerator {
     public static List<Student> createStudent(){
-        List<Student> students = new ArrayList<>();
-        students.add(new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(1,90),new Faker().address().state()) );
-        students.add(new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(1,90),new Faker().address().state()) );
-        students.add(new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(1,90),new Faker().address().state()) );
-        students.add(new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(1,90),new Faker().address().state()) );
-
+        List<Student> students = Arrays.asList(new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(0,80),new Faker().address().state()),
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(0,80),new Faker().address().state()),
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(0,80),new Faker().address().state()),
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(0,80),new Faker().address().state()));
         return students;
     }
 }
