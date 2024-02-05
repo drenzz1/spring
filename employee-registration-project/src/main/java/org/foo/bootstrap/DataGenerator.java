a220b381305a4feccd7552b6a21aa720f7d11c81
drenzz1 <you@example.com>
2024-02-06 00:27:28 +0100

    Employe Validation Module

---
+++ employee-registration-project/src/main/java/org/foo/bootstrap/DataGenerator.java
@@ -0,0 +1,71 @@
+package org.foo.bootstrap;
+
+import org.foo.model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataGenerator {
+    private static List<Employee> employeeList = new ArrayList<>();
+    public static void saveEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+
+    public static List<Employee> readAllEmployess(){
+        return employeeList;
+    }
+    public static List<String> getAllStates(){
+        List<String> states = new ArrayList<>();
+        states.add("Alabama");
+        states.add("Alaska");
+        states.add("Arizona");
+        states.add("Arkansas");
+        states.add("California");
+        states.add("Colorado");
+        states.add("Connecticut");
+        states.add("Delaware");
+        states.add("Florida");
+        states.add("Georgia");
+        states.add("Hawaii");
+        states.add("Idaho");
+        states.add("Illinois");
+        states.add("Indiana");
+        states.add("Iowa");
+        states.add("Kansas");
+        states.add("Kentucky");
+        states.add("Louisiana");
+        states.add("Maine");
+        states.add("Maryland");
+        states.add("Massachusetts");
+        states.add("Michigan");
+        states.add("Minnesota");
+        states.add("Mississippi");
+        states.add("Missouri");
+        states.add("Montana");
+        states.add("Nebraska");
+        states.add("Nevada");
+        states.add("New Hampshire");
+        states.add("New Jersey");
+        states.add("New Mexico");
+        states.add("New York");
+        states.add("North Carolina");
+        states.add("North Dakota");
+        states.add("Ohio");
+        states.add("Oklahoma");
+        states.add("Oregon");
+        states.add("Pennsylvania");
+        states.add("Rhode Island");
+        states.add("South Carolina");
+        states.add("South Dakota");
+        states.add("Tennessee");
+        states.add("Texas");
+        states.add("Utah");
+        states.add("Vermont");
+        states.add("Virginia");
+        states.add("Washington");
+        states.add("West Virginia");
+        states.add("Wisconsin");
+        states.add("Wyoming");
+        return states;
+    }
+}
