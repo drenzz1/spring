392b4dbb7cd8710a9fb4502d3801e13e07d8740c
drenzz1 <you@example.com>
2024-02-18 14:19:08 +0100

    Creating tables

--- spring-10-orm-tableCreation/src/main/java/org/foo/model/Account.java
+++ spring-10-orm-tableCreation/src/main/java/org/foo/model/Account.java
@@ -1,13 +1,12 @@
 package org.foo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 @MappedSuperclass
 public class Account {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String owner;
     private BigDecimal balance;
