fa90aec71fcda3290f274630ec9782f5a72c61ea
drenzz1 <you@example.com>
2024-02-21 23:08:29 +0100

    FLY WAY DEPENDENCY

--- spring-13-ORM-CinemaLab/src/main/java/org/foo/CinemaApplication.java
+++ spring-13-ORM-CinemaLab/src/main/java/org/foo/CinemaApplication.java
@@ -1,13 +1,23 @@
 package org.foo;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CinemaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
+    }
+    //qita e perdorim kur te perdorim flyway
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
     }
 
 }
