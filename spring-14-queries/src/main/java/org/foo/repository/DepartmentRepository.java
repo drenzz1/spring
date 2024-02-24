1e5cec117ce4e9e3a5871ff0ce7a9855292f823b
drenzz1 <you@example.com>
2024-02-24 15:23:25 +0100

    @Transactional

--- spring-14-queries/src/main/java/org/foo/repository/DepartmentRepository.java
+++ spring-14-queries/src/main/java/org/foo/repository/DepartmentRepository.java
@@ -1,6 +1,7 @@
 package org.foo.repository;
 
 import org.foo.model.Department;
+import org.foo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,7 @@ public interface DepartmentRepository extends JpaRepository<Department,String> {
     //Display all departments in the health division
     List<Department>findByDivision(String division);
     List<Department>findDistinctTop3ByDivisionContains(String pattern);
+    List<Department>retrieveDepartmentByDivision(String division);
+    List<Department>retrieveDepartmentByDivisionContain(String pattern);
 
 }
