3390833312387925bb3dc99358f0a85d04b29ef9
drenzz1 <you@example.com>
2024-03-11 14:50:41 +0100

    Review

---
+++ spring-review-IoC/src/main/java/bean_practice/FullTimeEmployee.java
@@ -0,0 +1,7 @@
+package bean_practice;
+
+public class FullTimeEmployee {
+    public void createAccount(){
+        System.out.println("Fulltime Employee is created");
+    }
+}
---
+++ spring-review-IoC/src/main/java/bean_practice/PartTimeEmployee.java
@@ -0,0 +1,7 @@
+package bean_practice;
+
+public class PartTimeEmployee {
+    public void partTimeEmployee(){
+        System.out.println("Part Employee is created ");
+    }
+}
