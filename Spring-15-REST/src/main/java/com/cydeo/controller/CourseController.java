3d7b590e92c673738468b0fe3c851fe496ad96b7
drenzz1 <you@example.com>
2024-03-20 14:49:10 +0100

    MY first API with Id

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.cydeo.controller;
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,10 @@ public class CourseController {
     @GetMapping()
     public List<CourseDTO> getAllCourses(){
         return  courseService.getCourses();
-
     }
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id")Long id){
+        return courseService.getCourseById(id);
+    }
+
 }
