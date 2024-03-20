74ec3619ab408ea6e450421d5e54cac305da7569
drenzz1 <you@example.com>
2024-03-20 14:40:08 +0100

    MY first API

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -2,94 +2,24 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController // @Controller + @ResponseBody
-@RequestMapping("/courses/api/v1")
+@RestController // Controller + ResponseBody
+@RequestMapping("/courses")
 public class CourseController {
-
     private final CourseService courseService;
 
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
     }
 
-    @GetMapping
+    @GetMapping()
     public List<CourseDTO> getAllCourses(){
-        List<CourseDTO> list = courseService.getCourses();
-        return list;
+        return  courseService.getCourses();
 
     }
-
-    @GetMapping("{id}")
-    public CourseDTO getCourseById(@PathVariable("id") Long courseId){
-        return courseService.getCourseById(courseId);
-    }
-
-    @GetMapping("category/{name}")
-    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
-        return courseService.getCoursesByCategory(category);
-
-    }
-
-    @PostMapping
-    public CourseDTO createCourse(@RequestBody CourseDTO course){
-        return courseService.createCourse(course);
-    }
-
-    @PutMapping("{id}")
-    public void updateCourse(@PathVariable("id") Long courseId,@RequestBody CourseDTO course){
-        courseService.updateCourse(courseId,course);
-    }
-
-    @DeleteMapping("{id}")
-    public void deleteCourseById(@PathVariable("id") Long courseId){
-        courseService.deleteCourseById(courseId);
-    }
-
-    @DeleteMapping
-    public void deleteCourses(){
-        courseService.deleteCourses();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
