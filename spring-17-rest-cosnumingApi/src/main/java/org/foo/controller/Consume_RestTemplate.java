ea9d1a014b9021924e9acc82d3db1db2df7d8cd1
drenzz1 <you@example.com>
2024-03-21 13:03:27 +0100

    Some name changes

--- spring-17-rest-cosnumingApi/src/main/java/org/foo/controller/Consume_RestTemplate.java
+++ spring-17-rest-cosnumingApi/src/main/java/org/foo/controller/Consume_RestTemplate.java
@@ -1,13 +1,14 @@
 package org.foo.controller;
 
 import org.foo.dto.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -33,6 +34,20 @@ public class Consume_RestTemplate {
         return template.getForObject(URL, Object.class,id);
     }
 
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeFromDummyApi(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> responseEntity = template.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET,entity, Object.class);
+
+        return  responseEntity;
+
+
+    }
 
 
 }
